5b4e94fa92e15f05dc12ce693407006941fe8a7d
RammySekham <ramansekham@gmail.com>
2020-11-18 12:36:05 +1100

    added new coding files

---
+++ settings.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+#Zipped Data folder name
+File_name = "RawDatasets.zip"
+
+# Folder in which zipped file is placed
+RAW_DATA_DIR = "Data"
+
+#Path to Extraction of files from Zip folder
+EXTRACTED_DIR = "RawDatasets"  # will be zipped folder name (RawDatasets.zip)
+
+#Path to processed data folder
+PROCESSED_DIR = "Processed"  # Create datafolder for processd data
+
+#Path to assembled data folder
+ASSEMBLED_DIR = "Assembled"  # Create datafolder for assembled data for modelling
